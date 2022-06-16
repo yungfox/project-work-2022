22a25e1f29e9204ef25849c8a700596c504b26c4
Nicola02000 <83706599+Nicola02000@users.noreply.github.com>
2022-06-16 17:06:42 +0200

    Update Bill through SQL Azure

--- ApplicativoSalvataggioMongoeCoda/Program.cs
+++ ApplicativoSalvataggioMongoeCoda/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using ApplicativoSalvataggioMongoeCoda.Services;
 
 namespace ApplicativoSalvataggioMongoeCoda
@@ -6,10 +7,14 @@ namespace ApplicativoSalvataggioMongoeCoda
     class Program
     {
         DBMongo dBMongo = new DBMongo();
-        static void Main(string[] args)
+        static AzureFunctionService azureFunctionService = new AzureFunctionService();
+        static async Task Main(string[] args)
         {
-            MqttService mqtt = new MqttService("localhost", "#");
-            mqtt.Subscribe();
+            //MqttService mqtt = new MqttService("localhost", "#");
+            //mqtt.Subscribe();
+
+            var res = await azureFunctionService.GetUpdatedBillings();
+            Console.WriteLine(res.seiore);
 
             //string test = $"{{\"_id\":\"55555555\",\"OrarioEntrata\":{{\"$date\":\"2022-06-15T11:19:35.397Z\"}},\"OrarioPagamento\":{{\"$date\":\"2022-06-15T11:21:35.398Z\"}},\"OrarioUscita\":{{\"$date\":\"1970-01-01T00:00:00.000Z\"}},\"Prezzo\":90}}";
 
---
+++ ApplicativoSalvataggioMongoeCoda/Services/AzureFunctionService.cs
@@ -0,0 +1,46 @@
+﻿using ApplicativoSalvataggioMongoeCoda.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicativoSalvataggioMongoeCoda.Services
+{
+    class AzureFunctionService
+    {
+        public Task<Prezzi> GetUpdatedBillings()
+        {
+            return Task.Run(() =>
+            {
+                using (SqlConnection con = new SqlConnection(Secrets.SQL_CONNECTION_STRING))
+                using (SqlCommand cmd = new SqlCommand()
+                {
+                    Connection = con,
+                    CommandType = System.Data.CommandType.Text,
+                    CommandText = "SELECT * FROM tblPrezzi"
+                })
+                {
+                    List<Prezzi> myListPrezzi = new List<Prezzi>();
+                    Prezzi myPrezzi;
+                    con.Open();
+                    SqlDataReader mySqlDataReader = cmd.ExecuteReader();
+                    while (mySqlDataReader.Read())//finchè c'è qualcosa da leggere, il metodo Read() restituisce true se trova una riga
+                    {
+                        myPrezzi = new Prezzi()
+                        {
+                            mezzora =(float)Convert.ToDouble(mySqlDataReader["mezzora"]),
+                            unora = (float)Convert.ToDouble(mySqlDataReader["unora"]),
+                            treore = (float)Convert.ToDouble(mySqlDataReader["treore"]),
+                            seiore = (float)Convert.ToDouble(mySqlDataReader["seiore"]),
+                            giornaliero = (float)Convert.ToDouble(mySqlDataReader["giornaliero"]),
+                        };
+                        myListPrezzi.Add(myPrezzi);
+                    }
+                    return myListPrezzi[0];
+                }
+            });
+        }
+    }
+}

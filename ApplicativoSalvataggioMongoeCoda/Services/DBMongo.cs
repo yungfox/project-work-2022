6cc03aaedc601e6e1c19677929823503f28a612a
Nicola02000 <83706599+Nicola02000@users.noreply.github.com>
2022-06-16 12:40:52 +0200

    ita to eng 1

--- ApplicativoSalvataggioMongoeCoda/Services/DBMongo.cs
+++ ApplicativoSalvataggioMongoeCoda/Services/DBMongo.cs
@@ -21,16 +21,16 @@ namespace ApplicativoSalvataggioMongoeCoda
             this.dbParcheggio = client.GetDatabase("Parcheggio");
         }
 
-        public void Entrata(string IDTicket)
+        public void Entry(string IDTicket)
         {
             try
             {
-                var orarioentrata = DateTime.UtcNow;
+                var entrytime = DateTime.UtcNow;
                 var collection = dbParcheggio.GetCollection<Biglietto>("Biglietti");
                 Biglietto myBiglietto = new()
                 {
                     IdBiglietto = IDTicket,
-                    OrarioEntrata = orarioentrata
+                    OrarioEntrata = entrytime
                 };
                 collection.InsertOne(myBiglietto);
             }
@@ -40,11 +40,11 @@ namespace ApplicativoSalvataggioMongoeCoda
             }
             
         }
-        public float Pagamento(string IDTicket)
+        public float Payment(string IDTicket)
         {
             try
             {
-                var orariopagamento = DateTime.UtcNow;
+                var paymentime = DateTime.UtcNow;
                 var collection = dbParcheggio.GetCollection<Biglietto>("Biglietti");
                 //calcolo quanto tempo è passato da quando sono entrato a quando voglio pagare
                 var filter = Builders<Biglietto>.Filter.Eq("IdBiglietto", IDTicket);
@@ -53,47 +53,47 @@ namespace ApplicativoSalvataggioMongoeCoda
                 if (DateTime.Compare(document.OrarioPagamento, Convert.ToDateTime("1970-01-01T00:00:00.000+00:00")) < 0)
                 {
                     //se l'orario pagamento non è mai stato istanziato allora calcolo
-                    diff = orariopagamento.Subtract(Convert.ToDateTime(document.OrarioEntrata));
+                    diff = paymentime.Subtract(Convert.ToDateTime(document.OrarioEntrata));
                 }
                 else
                 {
                     //se invece è già istanziato vuol dire che l'utente ha già pagato una volta ma ci ha messo troppo tempo ad arrivare all'uscita
-                    diff = orariopagamento.Subtract(Convert.ToDateTime(document.OrarioPagamento));
+                    diff = paymentime.Subtract(Convert.ToDateTime(document.OrarioPagamento));
                 }
                 //prendo i prezzi odierni
                 var collection2 = dbParcheggio.GetCollection<Prezzi>("Prezzi");
                 var filter2 = Builders<Prezzi>.Filter.Eq("id", 1);
                 var document2 = collection2.Find(filter2).First();
-                float prezzo = 0;
+                float price = 0;
                 switch (diff.TotalMinutes)
                 {
                     case <= 15:
-                        prezzo = 0;
+                        price = 0;
                         break;
                     case <= 30:
-                        prezzo = document2.mezzora;
+                        price = document2.mezzora;
                         break;
                     case <= 60:
-                        prezzo = document2.unora;
+                        price = document2.unora;
                         break;
                     case <= 180:
-                        prezzo = document2.treore;
+                        price = document2.treore;
                         break;
                     case <= 360:
-                        prezzo = document2.seiore;
+                        price = document2.seiore;
                         break;
                     case <= 1440:
-                        prezzo = Convert.ToInt32(diff.TotalDays) * document2.giornaliero;
+                        price = Convert.ToInt32(diff.TotalDays) * document2.giornaliero;
                         break;
                     default:
                         break;
                 }
                 filter = Builders<Biglietto>.Filter.Eq("IdBiglietto", IDTicket);
-                var update = Builders<Biglietto>.Update.Set("OrarioPagamento", orariopagamento);
+                var update = Builders<Biglietto>.Update.Set("OrarioPagamento", paymentime);
                 collection.UpdateOne(filter, update);
-                update = Builders<Biglietto>.Update.Set("Prezzo", prezzo + document.Prezzo);
+                update = Builders<Biglietto>.Update.Set("Prezzo", price + document.Prezzo);
                 collection.UpdateOne(filter, update);
-                return prezzo;
+                return price;
             }
             catch (Exception ex)
             {
@@ -105,17 +105,17 @@ namespace ApplicativoSalvataggioMongoeCoda
         {
             try
             {
-                var orariouscita = DateTime.UtcNow;
+                var exitime = DateTime.UtcNow;
                 var collection = dbParcheggio.GetCollection<Biglietto>("Biglietti");
                 //calcolo quanto tempo è passato da quando il cliente ha pagato a quando è arrivato alla sbarra
                 var filter = Builders<Biglietto>.Filter.Eq("IdBiglietto", IDTicket);
                 var document = collection.Find(filter).First();
-                System.TimeSpan diff = orariouscita.Subtract(document.OrarioPagamento);
+                System.TimeSpan diff = exitime.Subtract(document.OrarioPagamento);
                 //se sono passati meno di 15 minuti dal pagamento il cliente può uscire
                 if (diff.TotalMinutes <= 15)
                 {
                     filter = Builders<Biglietto>.Filter.Eq("IdBiglietto", IDTicket);
-                    var update = Builders<Biglietto>.Update.Set("OrarioUscita", orariouscita);
+                    var update = Builders<Biglietto>.Update.Set("OrarioUscita", exitime);
                     collection.UpdateOne(filter, update);
                     return true;
                 }

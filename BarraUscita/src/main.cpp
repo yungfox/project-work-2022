310ab585bb74cbb034176350f67eee2d854f4fe3
alemonteleale <96069240+alemonteleale@users.noreply.github.com>
2022-06-17 11:04:40 +0200

    EXIT ESP32 WITH SUBSCIBE TO A TOPIC

--- BarraUscita/src/main.cpp
+++ BarraUscita/src/main.cpp
@@ -4,12 +4,12 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <time.h>
+#include <ArduinoJson.h>
 
 //#define debug
 
 #define pinRst 22
 #define pinSs 5
-MFRC522 mfrc522(pinSs, pinRst);
 
 /* #region  Definizioni Variabili Per Cconnessione Wifi e Server Mqtt */
 // initialize constants
@@ -20,11 +20,105 @@ const char *TOPIC = MQTT_TOPIC;
 const int MQTT_PORT = 1883;
 const int PUBLISH_INTERVAL = 5000;
 /* #endregion */
-// instantiate wifi, pubsub and sensor clients
+
+/* #region Inizializzo gli oggetti  */
+
+MFRC522 mfrc522(pinSs, pinRst);
 WiFiClient espClient;
 PubSubClient client(espClient);
+/* #endregion */
 
+/* #region  funzione di callback per l'iscrizione */
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  // char input[MAX_INPUT_LENGTH];
+  Serial.println("Message arrived in topic: ");
+  Serial.println(topic);
+  Serial.println("Message:");
+  char js[length];
+  js[length]='\0';
+  for (int i = 0; i < length; i++)
+  {
+    js[i]=(char)payload[i];
+  }
+  //Serial.print(js);
+  StaticJsonDocument<32> doc;
 
+  DeserializationError error = deserializeJson(doc, js, length);
+
+  if (error) {
+    Serial.print("deserializeJson() failed: ");
+    Serial.println(error.c_str());
+    return;
+  }
+
+  const char* id = doc["_id"]; // "F4 AA B2 89"
+  const char* Dispositivo = doc["Dispositivo"]; // "ESP32Uscita"
+
+ 
+  Serial.println(Dispositivo);
+  Serial.println("-----------------------");
+}
+/* #endregion */
+
+/* #region  Invia Dati con ricezione di stringa */
+void InviaDati(char *datiEntrata)
+{
+  //client.connect("SbarraUscita");
+  if (client.connected())
+  {
+    char clientid[12];
+    String sbarra = "ESP32Uscita";
+    sbarra.toCharArray(clientid, 12);
+    // Serial.println(clientid);
+
+    char js[60];
+    js[0] = '\0';
+    strcat(js, "{\"_id\":\"");
+    strcat(js, datiEntrata);
+    strcat(js, "\",\"Dispositivo\":\"");
+    strcat(js, clientid);
+    strcat(js, "\"}");
+
+    // publish to the broker
+    client.publish(TOPIC, js);
+    Serial.print(js);
+  }
+  delay(PUBLISH_INTERVAL);
+  client.subscribe(TOPIC);
+}
+/* #endregion */
+
+/* #region  Lettura RFID  */
+void LetturaRFID()
+{
+  // Look for new cards
+  if (!mfrc522.PICC_IsNewCardPresent())
+  {
+    return;
+  }
+  // Select one of the cards
+  if (!mfrc522.PICC_ReadCardSerial())
+  {
+    return;
+  }
+  // Show UID on serial monitor
+  String IdBiglietto = "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++)
+  {
+    IdBiglietto.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    IdBiglietto.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  IdBiglietto.toUpperCase();
+  char charbuffer[12];
+  IdBiglietto.toCharArray(charbuffer, 12, 1);
+  //  di sicuro non debuggato da alvise
+  InviaDati(charbuffer);
+ 
+}
+
+/* #endregion */
 
 // Setup
 void setup()
@@ -40,6 +134,7 @@ void setup()
 
   // mi connetto al wifi
   WiFi.begin(SSID, PASSWORD);
+  WiFi.setHostname(HOSTNAME);
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
@@ -47,79 +142,34 @@ void setup()
   }
   Serial.println("Connected to the WiFi network!");
 
-  
-  
   // connect to the mqtt broker
   client.setServer(MQTT_BROKER, MQTT_PORT);
-}
 
-void InviaDati(char* franco)
-{
-    client.setServer(MQTT_BROKER, MQTT_PORT);
-    client.connect("SbarraUscita");
-    if(client.connected())
+  client.setCallback(callback);
+  while (!client.connected())
+  {
+    String client_id = "esp32-client-";
+    client_id += String(WiFi.macAddress());
+    Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
+    if (client.connect(client_id.c_str()))
     {
-      Serial.println("Inviato con Franco");
-      char clientid[12];
-      String sbarra = "ESP32Uscita";
-      sbarra.toCharArray(clientid, 12);
-      Serial.println(clientid);
-
-      char js[60];
-      js[0] = '\0';
-      strcat(js, "{\"_id\":\"");
-      strcat(js, franco);
-      strcat(js, "\",\"Dispositivo\":\"");
-      strcat(js, clientid);
-      strcat(js, "\"}");
-
-      //char *json = js;
-
-      // publish to the broker
-      client.publish(TOPIC, js);
-      Serial.print(js);
-      
-      sbarra="";
-
+      Serial.println("Public emqx mqtt broker connected");
+      //LetturaRFID();
     }
+    else
+    {
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(1000);
+    }
+  }
+  
+  //Serial.println("hi hi hi aaaaaaaaaaaaaahhhhhhhhhhh");
+  client.subscribe(TOPIC);
 }
-
-
 
 void loop()
 {
-
-  // Look for new cards
-  if (!mfrc522.PICC_IsNewCardPresent())
-  {
-    return;
-  }
-  // Select one of the cards
-  if (!mfrc522.PICC_ReadCardSerial())
-  {
-    return;
-  }
-  // Show UID on serial monitor
-  Serial.println("UID tag :");
-  String IdBiglietto = "";
-  byte letter;
-  for (byte i = 0; i < mfrc522.uid.size; i++)
-  {
-    IdBiglietto.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-    IdBiglietto.concat(String(mfrc522.uid.uidByte[i], HEX));
-  }
-  // c
-  IdBiglietto.toUpperCase();
-
-  // build message string by concatenating all readings separated by a comma
-  // char message[100];
-  char charbuffer[12];
-  IdBiglietto.toCharArray(charbuffer, 12, 1);
-  Serial.println(charbuffer);
-  // di sicuro non debuggato da alvise
-  InviaDati(charbuffer);
-
-
-  
-  delay(PUBLISH_INTERVAL);
+  LetturaRFID();
+  client.loop();
 }

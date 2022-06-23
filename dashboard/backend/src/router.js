67e93c8cb3d79161fecf6dd7d4fb154ac497a50c
yungfox <crifox12@gmail.com>
2022-06-23 11:28:54 +0200

    successfully tested retrieval of data from sqlserver from node

--- dashboard/backend/src/router.js
+++ dashboard/backend/src/router.js
@@ -1,12 +1,25 @@
 const express = require('express')
+const sql = require('mssql')
 const router = express.Router()
-const sql = require('./sqlserver')
+const sqlConfig = require('./sqlserver')
 
-// qui vanno definite le API. dovrebbero interrogare il 
-// database sql di azure e restituire i dati. verranno 
-// poi chiamate da vue per mostrare i dati nella pagina
-
-// questa è  solo un'api di test per vedere che funzioni
 router.get('/testapi', ({ res }) => res.send('yep'))
+
+router.get('/sqltest', (req, res) => {
+    sql.connect(sqlConfig, err => {
+        try {
+            let request = new sql.Request()
+            request.query('SELECT Id, Status FROM tblParkingSpot', (err, recordset) => {
+                res.end(JSON.stringify(recordset.recordsets[0]))
+            })
+            request.query()
+        } 
+        catch(err) {
+            console.log(err)
+            res.sendStatus(500)
+            return
+        }
+    })
+})
 
 module.exports = router
--- dashboard/backend/src/sqlserver.js
+++ dashboard/backend/src/sqlserver.js
@@ -1,64 +1,18 @@
-const tedious = require('tedious')
 const connectionString = process.env.SQL_CONNECTION_STRING
 const username = process.env.USERNAMESQL;
 const password = process.env.PASSWORDSQL;
 const SQLserver = process.env.YOUR_SERVER;
 
+const config = {  
+    "server": SQLserver,
+    "user": username,
+    "password": password,
+    "options": { "encrypt": true, "database": "dbParking" }
+};
+
+module.exports = config
+
 // logica di interrogazione del server...
-var Connection = tedious.Connection;  
-    var config = {  
-        server: SQLserver,  //update me
-        authentication: {
-            type: 'default',
-            options: {
-                userName: username, //update me
-                password: password  //update me
-            }
-        },
-        options: {
-            // If you are on Microsoft Azure, you need encryption:
-            encrypt: true,
-            database: 'dbParking'  //update me
-        }
-    };  
-    const connection = new Connection(config);
-
-    connection.connect((err) => {
-    if (err) {
-        console.log('Connection Failed');
-        throw err;
-    }
-
-    executeStatement();
-    });
-  
-    var Request = tedious.Request;  
-    var TYPES = tedious.TYPES;  
-  
-    function executeStatement() {  
-        request = new Request("SELECT Id,Status FROM tblParkingSpot for json path", function(err) {  
-        if (err) {  
-            console.log(err);}  
-        });  
-        var result = []; 
-        request.on('row', function(columns) {    
-            var obj = JSON.parse(columns);
-
-            console.log(obj);     
-        });  
-  
-        console.log(result);
-
-        request.on('done', function(rowCount, more) {  
-        console.log(rowCount + ' rows returned');  
-        });  
-        
-        // Close the connection after the final event emitted by the request, after the callback passes
-        request.on("requestCompleted", function (rowCount, more) {
-            connection.close();
-        });
-        connection.execSql(request);  
-    }  
 
 // https://docs.microsoft.com/en-us/sql/connect/node-js/step-3-proof-of-concept-connecting-to-sql-using-node-js?view=sql-server-ver16
 // https://github.com/tediousjs/tedious/tree/master/examples

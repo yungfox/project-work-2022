ce715b02e55d5598040ff183970c3801f829271b
Nicola02000 <83706599+Nicola02000@users.noreply.github.com>
2022-06-23 11:54:41 +0200

    Add query for api

--- dashboard/backend/src/router.js
+++ dashboard/backend/src/router.js
@@ -9,8 +9,8 @@ router.get('/sqltest', (req, res) => {
     sql.connect(sqlConfig, err => {
         try {
             let request = new sql.Request()
-            request.query('SELECT Id, Status FROM tblParkingSpot', (err, recordset) => {
-                res.end(JSON.stringify(recordset.recordsets[0]))
+            request.query('SELECT Id, Status FROM tblParkingSpot;SELECT AVG(DATEDIFF(MINUTE,EntryTime,ExitTime)) AS AVGtimestop FROM tblTicket;SELECT COUNT(Id) AS CountFreeSpot FROM tblParkingSpot WHERE Status = 1', (err, recordset) => {
+                res.end(JSON.stringify(recordset.recordsets))
             })
             request.query()
         } 

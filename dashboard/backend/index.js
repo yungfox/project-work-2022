bd04c7f1be117206523d698ca8648f4bcac60b1e
yungfox <crifox12@gmail.com>
2022-06-24 14:39:49 +0200

    moved gateway apps in a single folder

--- dashboard/backend/index.js
+++ dashboard/backend/index.js
@@ -1,12 +1,14 @@
 require('dotenv').config({path: __dirname + '/.env'})
 
+const path = require('path')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const router = require('./src/router')
 const websocket = require('ws')
 
-const connectionString = process.env.AZURE_CONNECTION_STRING
+const connectionString = process.env.YUNGTEST
+//const connectionString = process.env.AZURE_CONNECTION_STRING
 const device = require('azure-iot-device').Client
 const protocol = require('azure-iot-device-amqp').Amqp
 
@@ -18,18 +20,23 @@ wss.on('connection', async socket => {
     console.log('client connected')
     
     try {
-        client.open()
-        .then(() => {
-            console.log('connected to iothub!')
-            client.on('message', message => {
-                let body = message.getBytes().toString('ascii')
-                
-                console.log(`new message: ${body}`)
-    
-                socket.send(body)
-                client.complete(message)
-            })
+        client.on('message', message => {
+            let body = message.getBytes().toString('ascii')
+            
+            console.log(`new message: ${body}`)
+
+            socket.send(body)
+            client.complete(message)
         })
+
+        client.on('disconnect', () => {
+            client.removeAllListeners()
+            console.log('client disconnected')
+        })
+
+        client.open()
+        .then(() => console.log('connected to iothub!'))
+        .catch(() => client.close())
     } catch(err) {
         console.log(err)
     }
@@ -37,6 +44,11 @@ wss.on('connection', async socket => {
 
 app.use(cors())
 app.use(router)
+app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
+})
 
 const server = app.listen(3000, () => console.log('app listening on port 3000'))
 
